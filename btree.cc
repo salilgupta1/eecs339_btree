039374608cc3c57c3d608337c3802cec764c3440
Henry Spindell <henryspindell@gmail.com>
2014-11-24 21:38:01 -0600

    super updating

--- btree.cc
+++ btree.cc
@@ -238,10 +238,8 @@ ERROR_T BTreeIndex::LookupOrUpdateInternal(const SIZE_T &node,
 	if (op==BTREE_OP_LOOKUP) { 
 	  return b.GetVal(offset,value);
 	} else { 
-	  // BTREE_OP_UPDATE
-	  // WRITE ME
-	  return ERROR_UNIMPL;
-	}
+	  return b.SetVal(offset,value);
+	}     
       }
     }
     return ERROR_NONEXISTENT;
@@ -361,8 +359,7 @@ ERROR_T BTreeIndex::Insert(const KEY_T &key, const VALUE_T &value)
   
 ERROR_T BTreeIndex::Update(const KEY_T &key, const VALUE_T &value)
 {
-  // WRITE ME
-  return ERROR_UNIMPL;
+  return LookupOrUpdateInternal(superblock.info.rootnode, BTREE_OP_UPDATE, key, value);
 }
 
   
