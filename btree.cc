66ca7963bc312676d5fc27a34426d08b4ec8dd10
Kaustubh Garimella <kgf748@murphy.wot.ece.northwestern.edu>
2014-12-02 14:22:54 -0600

    Changed the const call on update

--- btree.cc
+++ btree.cc
@@ -359,7 +359,7 @@ ERROR_T BTreeIndex::Insert(const KEY_T &key, const VALUE_T &value)
   
 ERROR_T BTreeIndex::Update(const KEY_T &key, const VALUE_T &value)
 {
-  return LookupOrUpdateInternal(superblock.info.rootnode, BTREE_OP_UPDATE, key, value);
+  return LookupOrUpdateInternal(superblock.info.rootnode, BTREE_OP_UPDATE, key, (VALUE_T&) value);
 }
 
   
